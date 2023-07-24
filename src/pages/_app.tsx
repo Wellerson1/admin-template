6a1d33e6a9f7c2e31f2a275b3a74436899290969
Wellerson Gomes <wellersongomes@MacBook-Air-de-Wellerson.local>
2023-07-23 22:56:50 -0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import { AppProvider } from '../data/context/AppContext'
 import { AuthProvider } from '../data/context/AuthContext'
-import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -24,7 +24,6 @@ export default function Autenticacao() {
     
     function logincomgoogle() {
         debugger
-        
     }
     
     return (
