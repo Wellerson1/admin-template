76dfdfdbc92021485f3c4f9f1dffb0bedbb5542d
Wellerson Gomes <wellersongomes@MacBook-Air-de-Wellerson.local>
2023-08-19 17:12:28 -0300

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,13 @@
 import firebase from "firebase"
 
+const NEXT_PUBLIC_FIREBASE_API_KEY=AIzaSyASy1zX-K2PkzwoHEKLd-UbhoZCmhQiGJQ,
+NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN=admin-template-well.firebaseapp.com,
+NEXT_PUBLIC_FIREBASE_PROJECT_ID=admin-template-well
+
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: "admin-template-well.appspot.com",
     messagingSenderId: "1081175775253",
     appId: "1:1081175775253:web:cca2e6160a10e72c8a961c"
